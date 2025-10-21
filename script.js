e1a6cfd31c2a63b0dad95b2ae8e2e76a19d5b2dc
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-21 09:04:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let squares = [
+	document.getElementById("square1"),
+	document.getElementById("square2"),
+	document.getElementById("square3")
+]
+
+squares.forEach((item, index)=>{
+	item.addEventListener("mouseover", ()=>{
+		squares.forEach((s, i)=>{
+			s.style.backgroundColor = i === index ? "#E6E6FA" : "#6F4E37"; 
+		})
+	})
+
+	item.addEventListener("mouseout", ()=>{
+		squares.forEach((s, i)=>{
+			s.style.backgroundColor = "#E6E6FA" ; 
+		})
+	})
+})
